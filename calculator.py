434bf1949c72b5b8507573583b986086a4920925
Tom Halle <tom.halle01@gmail.com>
2024-04-12 15:14:46 +0200

    add soustraction and division

--- calculator.py
+++ calculator.py
@@ -1,3 +1,11 @@
 def addition(a,b):
     result = a + b
     return result
+
+def soustraction(a,b):
+    result = a - b
+    return result
+
+def division(a,b):
+    result = a/b
+    return result
