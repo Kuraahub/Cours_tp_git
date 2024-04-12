f765bdbfd574af5c94ce1b96e6120c0631e8ea5d
Tom Halle <tom.halle01@gmail.com>
2024-04-12 16:17:19 +0200

    add test

---
+++ test_calculator.py
@@ -0,0 +1,10 @@
+from calculator import addition, soustraction, division
+
+def test_addition():
+    assert addition(1, 2) == 3
+
+def test_soustraction():
+    assert soustraction(2,1) == 1
+
+def test_division();
+    asser division (1,1) == 1
